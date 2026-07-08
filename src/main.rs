8aa775160b658f7faaaf1eac3a9c17557c666adf
Nathan T <nathan@trioleyre.fr>
2026-07-08 22:34:11 +0200

    feat: replacing safetensors with gguf

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-use std::path::Path;
-
 use anyhow::Result;
 
-use crate::model::config::Config;
-
-mod json_helper;
-mod model;
-
 fn main() -> Result<()> {
-    let model_config_path = Path::new("data/SmolLM2-135M/config.json");
-    let model_config = json_helper::load::<Config>(model_config_path)?;
-
-    println!("{:?}", model_config);
-
     Ok(())
 }
